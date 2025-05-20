3d1b518c239eb384ad559ea8f2d69e1ff7a884ca
MTiergarten <mariatiergarten@gmail.com>
2025-05-20 19:10:47 +0200

    updated API key

--- spoonacular.api.js
+++ spoonacular.api.js
@@ -1,5 +1,7 @@
+// Spoonacular Doc: https://spoonacular.com/food-api/docs
+
 async function findVegetarian() {
-    const apiKey = '4b9edfca8285433db4ff655bddb9d300';
+    const apiKey = '582a62bc2bef47c9a0e3aededb18d8bc';
     let endpoint = 'https://api.spoonacular.com/recipes/complexSearch?apiKey=4b9edfca8285433db4ff655bddb9d300&diet=paleo'; // question mark at end of endpoint url (https://api.spoonacular.com/recipes/complexSearch) indicates that parameters are starting
     const response = await fetch(endpoint);
     const results = await response.json();

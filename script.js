c0c8c5e1f90a620b2a3014842837c5f4539cd808
MTiergarten <mariatiergarten@gmail.com>
2025-05-21 16:06:59 +0200

    renamed parameter (dietaryPreferences)

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ class Recipe { //classes should be named starting with capital letter
     static allRecipes = [];
     //used ChatGPT to understand how I could create a command or function inside my class "recipe" to updates my array of recipes when I create a new instance, instead of having to do this manually, so it suggested using the static property
 
-    constructor(name, type, ingredients, portions, categories) {
+    constructor(name, type, ingredients, portions, dietaryPreferences) {
         this.name = name;
         this.type = type;
         this.ingredients = ingredients;
         this.portions = portions;
-        this.categories = categories;
+        this.dietaryPreferences = dietaryPreferences;
         Recipe.allRecipes.push(this);
     }
 
@@ -17,7 +17,7 @@ class Recipe { //classes should be named starting with capital letter
         console.log(this.type.toString());
         console.log("Ingredients: " + this.ingredients.toString());
         console.log("Portions: " + this.portions);
-        console.log("Category: " + this.categories.toString());
+        console.log("Category: " + this.dietaryPreferences.toString());
     }
 
     static listRecipes() {
@@ -25,47 +25,49 @@ class Recipe { //classes should be named starting with capital letter
     }
 }
 
+const dietaryPreferences = ['dairy', 'gluten', 'nuts', 'seafood', 'vegetarian', 'vegan'];
+
 const eggs = new Recipe(
     'Eggs On Toast With Cheese',
     ['breakfast'],
     ['50g bread (1 bun or 2 slices of toast)', '2 eggs (scrambled or omelet)', '20g cheese of choice'],
     1);
-eggs.categories = ['nuts', 'seafood', 'vegetarian']
+eggs.dietaryPreferences = ['nuts', 'seafood', 'vegetarian']
 
 const couscous = new Recipe(
     'Corn Couscous With Cheese',
     ['breakfast'],
     ['40g corn couscous', '1 tsp butter', '60ml water', 'Salt to taste', '20g cheese of choice'],
     1);
-couscous.categories = ['gluten', 'nuts', 'seafood', 'vegetarian'];
+couscous.dietaryPreferences = ['gluten', 'nuts', 'seafood', 'vegetarian'];
 
 const oatmeal = new Recipe(
     'High Protein Oatmeal',
     ['breakfast'],
     ['1 banana', '120ml milk of choice', '1 tbsp peanut butter', '20g protein powder of choice', '25g oats'],
     1);
-oatmeal.categories = ['dairy', 'gluten', 'seafood', 'vegetarian', 'vegan'];
+oatmeal.dietaryPreferences = ['dairy', 'gluten', 'seafood', 'vegetarian', 'vegan'];
 
 const bolognese = new Recipe(
     'Simplified Bolognese Pasta',
     ['lunch', 'dinner'],
     ['250g whole wheat fusilli pasta', '400g minced meat', '200g tomato sauce', '1 tbsp tomato sauce', '1/2 finely diced onion', '1 grated clove of garlic', 'Season to taste: salt, black pepper, cayenne pepper, smoked paprika'],
     3);
-bolognese.categories = ['dairy', 'gluten', 'seafood', 'nuts'];
+bolognese.dietaryPreferences = ['dairy', 'gluten', 'seafood', 'nuts'];
 
 const chicken = new Recipe(
     'Grilled Chicken with Herb Potatoes',
     ['lunch', 'dinner'],
     ['400g chicken breast (butterflied)', '1 lime', '1 grated clove or garlic', '2 tbsp olive oil', 'Season to taste: salt and black pepper', '700g potatoes, cut into 1 inch cubes: season to taste with wild garlic, pesto or parsley after boiling'],
     3);
-chicken.categories = ['dairy', 'gluten', 'seafood', 'nuts'];
+chicken.dietaryPreferences = ['dairy', 'gluten', 'seafood', 'nuts'];
 
 const pork = new Recipe(
     'Roasted Pork Tenderloin with Mashed Potatoes',
     ['lunch', 'dinner'],
     ['700g potatoes (peeled and quartered)', '30g butter', '100ml milk of choice', 'Season to taste: salt, nutmeg, garlic powder and black pepper', '400g pork tenderloin', '2 tbsp olive oil', '2 cloves garlic', '1 tsp paprika', 'Season to taste: salt and black pepper'],
     3);
-pork.categories = ['gluten', 'seafood', 'nuts'];
+pork.dietaryPreferences = ['gluten', 'seafood', 'nuts'];
 
 const wrap = new Recipe(
     'Chicken Wrap',
@@ -80,46 +82,47 @@ const salad1 = new Recipe(
     ['veggies'],
     ['Mixed fresh green leaves', 'Olive oil and balsamic dressing'],
     1);
-salad1.categories = ['dairy', 'gluten', 'nuts', 'seafood', 'vegetarian', 'vegan'];
+salad1.dietaryPreferences = ['dairy', 'gluten', 'nuts', 'seafood', 'vegetarian', 'vegan'];
 
 const salad2 = new Recipe(
     'Mixed Salad',
     ['veggies'],
     ['Diced tomatoes', 'Sliced cucumber', 'Sliced red onion', 'Sliced bell peppers', 'Lettuce or arugula', 'Olive oil and apple cider vinegar dressing'],
     1);
-salad2.categories = ['dairy', 'gluten', 'nuts', 'seafood', 'vegetarian', 'vegan'];
+salad2.dietaryPreferences = ['dairy', 'gluten', 'nuts', 'seafood', 'vegetarian', 'vegan'];
 
 const veggies = new Recipe(
     'Steamed Veggie Mix',
     ['veggies'],
     ['Mixed veggies fresh or frozen', 'Season to taste: salt, black pepper, paprika'],
     1);
-veggies.categories = ['dairy', 'gluten', 'nuts', 'seafood', 'vegetarian', 'vegan'];
+veggies.dietaryPreferences = ['dairy', 'gluten', 'nuts', 'seafood', 'vegetarian', 'vegan'];
 
 const tuna = new Recipe(
     'Tuna Sandwich',
     ['snack'],
     ['1 can of tuna (in brine)', '2 tbsp light teriyaki sauce', '2 tbsp light mayo', '4 slices of toast', 'Optional: lettuce, sliced red onion'],
     2);
-tuna.categories = ['dairy', 'nuts'];
+tuna.dietaryPreferences = ['dairy', 'nuts'];
 
 const yoghurt = new Recipe(
     'Yoghurt Bowl with Fruits',
     ['snack'],
     ['150g greek yoghurt', '15g protein of choice', '150g fruit of choice', 'Optional: 1 tbsp honey'],
     1);
-yoghurt.categories = ['gluten', 'nuts', 'seafood', 'vegetarian'];
+yoghurt.dietaryPreferences = ['gluten', 'nuts', 'seafood', 'vegetarian'];
 
 const grilledCheese = new Recipe(
     'Grilled Cheese',
     ['snack'],
     ['2 slices of toast', '25g soft cheese of choice', '1 tsp butter'],
     1);
-grilledCheese.categories = ['nuts', 'seafood', 'vegetarian'];
+grilledCheese.dietaryPreferences = ['nuts', 'seafood', 'vegetarian'];
 
 
 const userPreferences = document.getElementById('userPreferences');
 const checkRestrictions = userPreferences.querySelectorAll('input[type="checkbox"]');
+//used chatGPT to understand the difference between elements that can / can't be iterable in a DOM manipulation - and how to create a variable to target all the checkboxes in my form to run in a loop
 // const recipesFound = document.getElementById('recipesFound');
 const mealPlan = document.getElementById('mealPlan');
 const breakfastTitle = document.getElementById('breakfastTitle');
@@ -130,7 +133,16 @@ const snackTitle = document.getElementById('snackTitle');
 const snack = document.getElementById('snack');
 const dinnerTitle = document.getElementById('dinnerTitle');
 const dinner = document.getElementById('dinner');
-//used chatGPT to understand the difference between elements that can / can't be iterable in a DOM manipulation - and how to create a variable to target all the checkboxes in my form to run in a loop
+
+
+function randomize(arr) {
+    for (let i = arr.length -1; i > 0; i--) {
+        let randomIndex = Math.floor(Math.random() * (i+1));
+        let currentElement = arr[i];
+        arr[i] = arr[randomIndex];
+        arr[randomIndex] = currentElement;
+    }
+}
 
 userPreferences.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -142,17 +154,8 @@ userPreferences.addEventListener('submit', function(event) {
         }
     }
 
-    let filteredRecipes = Recipe.allRecipes.filter(recipe => restrictions.every(restriction => recipe.categories.includes(restriction)));
+    let filteredRecipes = Recipe.allRecipes.filter(recipe => restrictions.every(restriction => recipe.dietaryPreferences.includes(restriction)));
     //used chatGPT to understand how I could use each item in the array as a condition for my filter - it suggested using the 'every' method
-
-    function randomize(arr) {
-        for (let i = arr.length -1; i > 0; i--) {
-            let randomIndex = Math.floor(Math.random() * (i+1));
-            let currentElement = arr[i];
-            arr[i] = arr[randomIndex];
-            arr[randomIndex] = currentElement;
-        }
-    }
 
     randomize(filteredRecipes);
 
@@ -220,7 +223,7 @@ userPreferences.addEventListener('submit', function(event) {
         }
     }
 
-    snackTitle.innerHTML = `<h3>Snack</h3>`;
+    snackTitle.innerHTML = `<h3>Snacks</h3>`;
     const snackContainer = document.getElementById('snack');
     snackContainer.innerHTML = '';
     snack.classList.add('meal-container');
